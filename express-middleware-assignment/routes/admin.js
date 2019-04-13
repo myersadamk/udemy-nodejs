2a26ea2ae459ddbb38d141d00ec501e3c75a7c4f
Adam Myers <myers.adam.k@gmail.com>
2019-04-13 15:33:34 -0400

    Finalized .ejs for nav-bar

--- express-middleware-assignment/routes/admin.js
+++ express-middleware-assignment/routes/admin.js
@@ -1,7 +1,9 @@
 const adminRoutes = require('express').Router();
 
 adminRoutes.get('/add-users', (req, res) => {
-  res.render('add-users.ejs');
+  res.render('add-users.ejs', {
+    target: '/admin/add-users'
+  });
 });
 
 adminRoutes.post('/add-users', (req, res) => {
--- express-middleware-assignment/routes/generic-routes.js
+++ express-middleware-assignment/routes/generic-routes.js
@@ -3,12 +3,16 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {
+    target: '/'
+  });
 });
 
 routes.use((req, res) => {
   res.status(404);
-  res.render('page-not-found.ejs');
+  res.render('page-not-found.ejs', {
+    target: undefined
+  });
 });
 
 module.exports = routes;
